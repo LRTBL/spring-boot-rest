5683bf191d7511bde442c202fb90ed77f177bd77
Fernando Mahiler Chullo Mamani <fernando.chullo@culqi.com>
2021-12-30 23:17:35 -0500

    [UPDATE]: Mapping Methods

--- src/main/java/com/lrtbl/helloworld/rest/controllers/UserController.java
+++ src/main/java/com/lrtbl/helloworld/rest/controllers/UserController.java
@@ -5,9 +5,7 @@ import com.lrtbl.helloworld.rest.services.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,8 +17,29 @@ public class UserController {
     private UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> get() {
-        return new ResponseEntity<>(userService.getUsers(), HttpStatus.OK);
+    public ResponseEntity<List<User>> getUser(@RequestParam(value="startWith", required = false) String startWith) {
+        return new ResponseEntity<>(userService.getUsers(startWith), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{username}")
+    public ResponseEntity<User> getUserByUsername (@PathVariable("username") String username) {
+        return new ResponseEntity<>(userService.getUserByUsername(username), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser (@RequestBody User user) {
+        return new ResponseEntity<>(userService.createUser(user), HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/{username}")
+    public ResponseEntity<User> updateUser (@PathVariable("username") String username, @RequestBody User user) {
+        return new ResponseEntity<>(userService.updateUser(user, username), HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{username}")
+    public ResponseEntity<Void> deleteUser (@PathVariable("username") String username){
+        userService.deleteUser(username);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
--- src/main/java/com/lrtbl/helloworld/rest/services/UserService.java
+++ src/main/java/com/lrtbl/helloworld/rest/services/UserService.java
@@ -3,11 +3,14 @@ package com.lrtbl.helloworld.rest.services;
 import com.github.javafaker.Faker;
 import com.lrtbl.helloworld.rest.models.User;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -18,12 +21,42 @@ public class UserService {
 
     @PostConstruct
     public void init() {
-        for (int i=0; i<100; i++) {
-            users.add(new User(faker.funnyName().name(), faker.name().username(), faker.dragonBall().character()));
-        }
+      for (int i = 0; i < 100; i++) {
+        users.add(
+            new User(
+                faker.funnyName().name(), faker.name().username(), faker.dragonBall().character()));
+      }
     }
 
-    public List<User> getUsers () {
-        return users;
+    public List<User> getUsers(String startWith) {
+      if (startWith != null)
+        return users.stream()
+            .filter(user -> user.getUsername().startsWith(startWith))
+            .collect(Collectors.toList());
+      return users;
+    }
+
+    public User getUserByUsername (String username) {
+        return users.stream().filter(user -> user.getUsername().equals(username)).findAny().orElseThrow(()-> new ResponseStatusException(HttpStatus.BAD_REQUEST, String.format("User %s not found", username)));
+    }
+
+    public User createUser (User user){
+        if(users.stream().anyMatch(u -> u.getUsername().equals(user.getUsername()))){
+            throw new ResponseStatusException(HttpStatus.CONFLICT, String.format("User %s not found", user.getUsername()));
+        }
+        users.add(user);
+        return user;
+    }
+
+    public User updateUser (User user, String userName){
+        User userToBeUpload = getUserByUsername(userName);
+        userToBeUpload.setNickName(user.getNickName());
+        userToBeUpload.setPassword(user.getPassword());
+        return userToBeUpload;
+    }
+
+    public void deleteUser (String username) {
+        User userToBeDelete = getUserByUsername(username);
+        users.remove(userToBeDelete);
     }
 }
